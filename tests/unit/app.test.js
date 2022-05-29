1cdb5bc9b9e2c8f197fe70254683db868b0fb889
Moxa Panchal <84884090+moxaPanchal@users.noreply.github.com>
2022-05-29 03:51:12 -0400

    add unit tests

---
+++ tests/unit/app.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('App test', () => {
+  test('should return HTTP 404 response', async () => {
+    const res = await request(app).get('/home');
+    expect(404);
+  });
+
+  test('should return status: error in response', async () => {
+    const res = await request(app).get('/fragments');
+    expect(res.body.status).toEqual('error');
+  });
+
+  test('should return a error message', async () => {
+    const res = await request(app).get('/fragments');
+    // expect(res.statusCode).toBe(404);
+    // expect(res.body.status).toEqual('error');
+    // expect(res.error.message).toEqual('not found');
+    expect(res.error.).toEqual('not found');
+  });
+});
